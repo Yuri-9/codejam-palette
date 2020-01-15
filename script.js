43a3525c2105cb11e772ec5bec73188b50b1d22d
Yuri-9 <vashkevichyp@gmail.com>
2020-01-16 01:58:42 +0300

    fix: add storage, small function, fix a lot issues

--- script.js
+++ script.js
@@ -1,156 +1,201 @@
-/* eslint-disable no-return-assign */
-/* eslint-disable no-plusplus */
-
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
 let isDrawing = false;
 let squireX = 0;
 let squireY = 0;
 const scale = 4;
 const scaleNumber = 512 / scale;
+let currentTool = 'pencil';
+let colorSelected = localStorage.getItem('colorSelected') || '#00fff0';
+const objectColor = {
+  currentColor: colorSelected,
+  prevColor: localStorage.getItem('prevColor') || '#00ff00',
+  redColor: '#ff0000',
+  blueColor: '#0000ff',
+};
 
+document.getElementById('currentColorBackground').style.backgroundColor = colorSelected;
+document.getElementById('prevColorBackground').style.backgroundColor = objectColor.prevColor;
+document.getElementById('redColorBackground').style.backgroundColor = objectColor.redColor;
+document.getElementById('blueColorBackground').style.backgroundColor = objectColor.blueColor;
 
-let colorGlobal;
-let prevColor;
-colorGlobal = localStorage.getItem('colorSocket1');
-prevColor = localStorage.getItem('colorSocket2');
-// eslint-disable-next-line eqeqeq
-if (colorGlobal == undefined) {
-  colorGlobal = '#ffff00';
-  prevColor = '#00ff40';
-}
-
-document.querySelector('#red').style.backgroundColor = '#ff0000';
-document.querySelector('#blue').style.backgroundColor = '#0000ff';
-document.querySelector('#current_color').style.backgroundColor = colorGlobal;
-document.querySelector('#prev_color').style.backgroundColor = prevColor;
-
-
-function chooseTools(event) {
-  const chooseToolEL = event.target.classList;
-  document.querySelector('.menu_tools .clickBtn').classList.remove('clickBtn');
-  chooseToolEL.add('clickBtn');
-}
-
-function chooseToolsKeyboard(event) {
-  if (event.code === 'KeyF') {
-    document.querySelector('.menu_tools .clickBtn').classList.remove('clickBtn');
-    document.querySelector('.menu_tools--btn_fill_bucket').classList.add('clickBtn');
-  }
-  if (event.code === 'KeyC') {
-    document.querySelector('.menu_tools .clickBtn').classList.remove('clickBtn');
-    document.querySelector('.menu_tools--btn_choose_color').classList.add('clickBtn');
-  }
-  if (event.code === 'KeyP') {
-    document.querySelector('.menu_tools .clickBtn').classList.remove('clickBtn');
-    document.querySelector('.menu_tools--btn_pencil').classList.add('clickBtn');
-  }
-}
-
-function chooseColor(event) {
-  window.oninput = function oninputColor() {
-    prevColor = document.querySelector('#current_color').style.backgroundColor;
-    document.querySelector('#prev_color').style.backgroundColor = prevColor;
-    const inputColor = document.getElementById('current_color_input').value;
-    document.querySelector('#current_color').style.backgroundColor = inputColor;
-    colorGlobal = inputColor;
-    localStorage.setItem('colorSocket1', colorGlobal);
-    localStorage.setItem('colorSocket2', prevColor);
+(function initCanvasAfterReload() {
+  const dataURL = localStorage.getItem('imageDataURL');
+  const img = new Image();
+  img.src = dataURL;
+  img.onload = () => {
+    ctx.drawImage(img, 0, 0);
   };
+}());
 
-  const colorEv = event.target;
-  if (colorEv.tagName === 'DIV') return;
-  document.querySelector('.menu_color .clickBtn').classList.remove('clickBtn');
-  colorEv.classList.add('clickBtn');
-  colorGlobal = colorEv.previousElementSibling.style.backgroundColor;
+function removeClassClickBtn(className) {
+  const arrayElements = document.querySelectorAll(className);
+  arrayElements.forEach((item) => item.classList.remove('clickBtn'));
 }
 
-function chooseSize(event) {
-  document.querySelector('.menu_size .clickBtn').classList.remove('clickBtn');
+function clearCanvas() {
+  ctx.clearRect(0, 0, 512, 512);
+}
+
+function setTool(event) {
+  const elementTool = event.target;
+  removeClassClickBtn('.menu_tools--item');
+  elementTool.classList.add('clickBtn');
+  currentTool = elementTool.id;
+  if (currentTool === 'clear') clearCanvas();
+}
+
+function setToolKeyboard(element) {
+  const elementTool = document.getElementById(element);
+  removeClassClickBtn('.menu_tools--item');
+  elementTool.classList.add('clickBtn');
+  currentTool = elementTool.id;
+}
+
+function keyboardHandler(event) {
+  switch (event.code) {
+    case 'KeyF':
+      setToolKeyboard('bucket');
+      break;
+    case 'KeyC':
+      setToolKeyboard('picker');
+      break;
+    case 'KeyP':
+      setToolKeyboard('pencil');
+      break;
+    default:
+  }
+}
+
+function getInputColor(event) {
+  const { value } = event.target;
+  objectColor.prevColor = objectColor.currentColor;
+  objectColor.currentColor = value;
+
+  const { currentColor, prevColor } = objectColor;
+
+  document.getElementById('currentColorBackground').style.background = currentColor;
+  document.getElementById('prevColorBackground').style.background = prevColor;
+
+  colorSelected = currentColor;
+}
+
+function setColor(event) {
+  const itemColor = event.target.closest('LI');
+  removeClassClickBtn('.menu_color--item');
+  itemColor.classList.add('clickBtn');
+  colorSelected = objectColor[itemColor.id];
+}
+
+function setSize(event) {
+  removeClassClickBtn('.menu_size--item');
   event.target.classList.add('clickBtn');
 }
 
 function drawPencil(event) {
-  if (document.querySelector('.menu_tools--btn_pencil').classList.length !== 3) return;
   if (!isDrawing) return;
-  for (let i = 0; i <= scale; i++) {
-    if (scaleNumber * i - event.offsetX < 0) {
-      squireX = i * scaleNumber;
+  if (currentTool === 'pencil') {
+    for (let i = 0; i <= scale; i += 1) {
+      if (scaleNumber * i - event.offsetX < 0) {
+        squireX = i * scaleNumber;
+      }
+      if (scaleNumber * i - event.offsetY < 0) {
+        squireY = i * scaleNumber;
+      }
     }
-    if (scaleNumber * i - event.offsetY < 0) {
-      squireY = i * scaleNumber;
-    }
+    ctx.fillStyle = colorSelected;
+    ctx.fillRect(squireX, squireY, scaleNumber, scaleNumber);
   }
-  ctx.fillStyle = colorGlobal;
-  ctx.fillRect(squireX, squireY, scaleNumber, scaleNumber);
 }
 
-function hexToRGB(hex) {
-  const r = parseInt(hex.slice(1, 3), 16);
-  const g = parseInt(hex.slice(3, 5), 16);
-  const b = parseInt(hex.slice(5, 7), 16);
-  return `rgb(${r}, ${g}, ${b})`;
+function changeHexToRGBA(hexColor) {
+  const r = parseInt(hexColor.slice(1, 3), 16);
+  const g = parseInt(hexColor.slice(3, 5), 16);
+  const b = parseInt(hexColor.slice(5, 7), 16);
+  return [r, g, b, 255];
 }
 
 function fillColor(event) {
-  if (document.querySelector('.menu_tools--btn_fill_bucket').classList.length !== 3) return;
-  canvas = document.getElementById('canvas');
-  ctx = canvas.getContext('2d');
-  const xEl = event.offsetX;
-  const yEl = event.offsetY;
-  const rgbaEl = ctx.getImageData(xEl, yEl, 1, 1).data;
-  let rgbaStringEl = `rgb(${rgbaEl[0]}, ${rgbaEl[1]}, ${rgbaEl[2]})`;
-  let colorGlobalRbg;
-  if (rgbaStringEl === 'rgb(0,0,0)' && (colorGlobal[0] === '#')) {
-    rgbaStringEl = 'rgb(255,255,255)';
-    colorGlobalRbg = hexToRGB(colorGlobal);
-  } else {
-    colorGlobalRbg = colorGlobal;
-  }
-  if (rgbaStringEl !== colorGlobalRbg) {
-    const fillArea = function fillArea() {
-      for (let x = 0; x < scale; x++) {
-        for (let y = 0; y < scale; y++) {
-          const pixelCanvasColor = ctx.getImageData((x * scaleNumber + 1),
-            (y * scaleNumber + 1), 1, 1).data;
-          const pixelCanvasColorRgb = `rgb(${pixelCanvasColor[0]}, ${pixelCanvasColor[1]}, ${pixelCanvasColor[2]})`;
-          if (pixelCanvasColorRgb === rgbaStringEl) {
-            ctx.fillStyle = colorGlobal;
-            ctx.fillRect(x * scaleNumber, y * scaleNumber, scaleNumber, scaleNumber);
-          }
-        }
-      }
-    };
+  const colorPixel = ctx.getImageData(event.offsetX, event.offsetY, 1, 1);
 
-    fillArea();
+  const colorSelectedRGBA = changeHexToRGBA(colorSelected);
+  const canvasData = ctx.getImageData(0, 0, 512, 512);
+
+  for (let i = 0; i < canvasData.data.length; i += 4) {
+    if (colorPixel.data.every((item, ind) => item === canvasData.data[i + ind])) {
+      for (let j = 0; j < 4; j += 1) {
+        canvasData.data[i + j] = colorSelectedRGBA[j];
+      }
+    }
+  }
+  ctx.putImageData(canvasData, 0, 0);
+}
+
+function changeRgbaToHex(rgbaData) {
+  let [r, g, b, a] = rgbaData;
+  r = r.toString(16);
+  g = g.toString(16);
+  b = b.toString(16);
+  a = a.toString(16);
+  if (r.length === 1) r = `0${r}`;
+  if (g.length === 1) g = `0${g}`;
+  if (b.length === 1) b = `0${b}`;
+  if (a.length === 1) a = `0${a}`;
+  return `#${r}${g}${b}${a}`;
+}
+
+function chooseColorPicker(event) {
+  removeClassClickBtn('.menu_color--item');
+  document.getElementById('currentColor').classList.add('clickBtn');
+
+  const { offsetX, offsetY } = event;
+  const rgbaData = ctx.getImageData(offsetX, offsetY, 1, 1).data;
+
+  let colorNewHex = changeRgbaToHex(rgbaData);
+  if (colorNewHex === colorSelected) return;
+  if (colorNewHex === '#00000000') {
+    colorNewHex = '#ffffffff';
+  }
+  colorSelected = colorNewHex;
+  objectColor.prevColor = objectColor.currentColor;
+  objectColor.currentColor = colorNewHex;
+
+  document.getElementById('currentColorBackground').style.background = colorNewHex;
+  document.getElementById('prevColorBackground').style.background = objectColor.prevColor;
+}
+
+function setLocalStorage() {
+  const imageDataURL = canvas.toDataURL();
+
+  localStorage.setItem('imageDataURL', imageDataURL);
+  localStorage.setItem('colorSelected', colorSelected);
+  localStorage.setItem('prevColor', objectColor.prevColor);
+}
+
+function mousedownHandler(event) {
+  isDrawing = true;
+  switch (currentTool) {
+    case 'bucket':
+      fillColor(event);
+      break;
+    case 'picker':
+      chooseColorPicker(event);
+      break;
+    case 'pencil':
+      drawPencil(event);
+      break;
+    default:
   }
 }
 
-// eslint-disable-next-line func-names
-canvas.onmousedown = function (event) {
-  if (document.querySelector('.menu_tools--btn_choose_color').classList.length !== 3) return;
-  const x = event.offsetX;
-  const y = event.offsetY;
-  const rgba = ctx.getImageData(x, y, 1, 1).data;
-  let rgbaString = `rgba(${rgba[0]},${rgba[1]},${rgba[2]},${rgba[3]})`;
-  if (rgbaString === 'rgba(0,0,0,0)') {
-    rgbaString = 'rgba(255,255,255,255)';
-  }
-
-  document.querySelector('.menu_color .clickBtn').classList.remove('clickBtn');
-  document.querySelector('#current_color_label').classList.add('clickBtn');
-
-  document.querySelector('#current_color').style.backgroundColor = rgbaString;
-  colorGlobal = rgbaString;
-};
-
 canvas.addEventListener('mousemove', drawPencil);
-canvas.addEventListener('mouseup', () => isDrawing = false);
-canvas.addEventListener('mousedown', () => isDrawing = true);
+canvas.addEventListener('mouseup', () => { isDrawing = false; });
+canvas.addEventListener('mousedown', mousedownHandler);
 
-document.querySelector('.menu_tools').addEventListener('mousedown', chooseTools);
-document.querySelector('.menu_color').addEventListener('mousedown', chooseColor);
-document.querySelector('.menu_size').addEventListener('mousedown', chooseSize);
-document.addEventListener('keydown', chooseToolsKeyboard);
+document.querySelector('.menu_tools').addEventListener('mousedown', setTool);
+document.querySelector('.menu_color').addEventListener('mousedown', setColor);
+document.querySelector('.menu_size').addEventListener('mousedown', setSize);
 
-canvas.addEventListener('mousedown', fillColor);
+document.addEventListener('keydown', keyboardHandler);
+window.addEventListener('input', getInputColor);
+window.addEventListener('beforeunload', setLocalStorage);
